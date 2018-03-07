87290fcf3d399d6d169cd8dc6c4f095ee29f82e3
jmirtsch <jonm@geometrygym.com>
2018-03-08 00:02:39 +1100

    Initial display of browser in panel

--- SpeckleRevitPlugin/Entry/ExtCmd.cs
+++ SpeckleRevitPlugin/Entry/ExtCmd.cs
@@ -9,7 +9,7 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 
-using CefSharp;
+//using CefSharp;
 using System.IO;
 using System.Reflection;
 //using CefSharp.WinForms;
@@ -33,18 +33,25 @@ namespace SpeckleRevitPlugin
             AppMain.Settings = new SettingsHelper(commandData);
 
             // SHOW DOCKABLE WINDOW
-            //DockablePaneId m_dpID = GlobalHelper.MainDockablePaneId;
-            //DockablePane m_dp = commandData.Application.GetDockablePane(m_dpID);
-            //m_dp.Show();
+            DockablePaneId m_dpID = GlobalHelper.MainDockablePaneId;
+            DockablePane m_dp = commandData.Application.GetDockablePane(m_dpID);
+			var path = Directory.GetParent(Assembly.GetExecutingAssembly().Location);
+			Debug.WriteLine(path, "SPK");
 
-            // initialise cef
-            if (!Cef.IsInitialized)
-                InitializeCef();
+			var indexPath = string.Format(@"{0}\app\index.html", path);
+
+			if (!File.Exists(indexPath))
+				Debug.WriteLine("Speckle for Revit: Error. The html file doesn't exists : {0}", "SPK");
+
+			indexPath = indexPath.Replace("\\", "/");
+			AppMain.MainDock.webBrowser.Source = new Uri( indexPath);
+			m_dp.Show();
+
 
             // initialise one browser instance
-            InitializeChromium();
+            //InitializeChromium();
 
-            var form = new SpeckleRevitForm();
+            //var form = new SpeckleRevitForm();
 
             //form.Controls.Add(Browser);
             //form.Show();
@@ -52,27 +59,6 @@ namespace SpeckleRevitPlugin
             return Result.Succeeded;
         }
 
-        void InitializeCef()
-        {
-
-            Cef.EnableHighDPISupport();
-
-            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
-            var assemblyPath = Path.GetDirectoryName(assemblyLocation);
-            var pathSubprocess = Path.Combine(assemblyPath, "CefSharp.BrowserSubprocess.exe");
-            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
-            var settings = new CefSettings
-            {
-                LogSeverity = LogSeverity.Verbose,
-                LogFile = "ceflog.txt",
-                BrowserSubprocessPath = pathSubprocess
-            };
-
-            // Initialize cef with the provided settings
-
-            Cef.Initialize(settings);
-
-        }
 
         public void InitializeChromium()
         {
