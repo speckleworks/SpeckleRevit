3dc3f5aa90d197f918c2aa0560ebbd1fb9678faa
Luis Fraguada <fraguada@gmail.com>
2018-03-10 12:37:23 +0100

    Starting Interop. #4.

---
+++ SpeckleRevitPlugin/Speckle/ISpeckleClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace SpeckleRhino
+{
+    /// <summary>
+    /// Generalises some methhods for both senders and receivers.
+    /// </summary>
+    public interface ISpeckleClient : IDisposable, ISerializable
+    {
+        SpeckleCore.ClientRole GetRole();
+
+        string GetClientId();
+
+        void TogglePaused(bool status);
+
+        void ToggleVisibility(bool status);
+
+        void ToggleLayerVisibility(string layerId, bool status);
+
+        void ToggleLayerHover(string layerId, bool status);
+
+        void Dispose(bool delete = false);
+    }
+}
