33cee2df92001989b86dae63eba81faca8158590
Luis Fraguada <fraguada@gmail.com>
2018-03-10 12:03:27 +0100

    Fixed issues with CefSharpWPF by initializing Cef from the form cs. Fixes #11.

--- SpeckleRevitPlugin/Entry/ExtCmd.cs
+++ SpeckleRevitPlugin/Entry/ExtCmd.cs
@@ -1,21 +1,9 @@
 ﻿#region Namespaces
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 
-using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
 
-using CefSharp;
-using CefSharp.WinForms;
-
-using System.IO;
-using System.Reflection;
-
-using System.Windows.Forms;
 #endregion
 
 namespace SpeckleRevitPlugin
@@ -23,7 +11,7 @@ namespace SpeckleRevitPlugin
     [Transaction(TransactionMode.Manual)]
     public class ExtCmd : IExternalCommand
     {
-        public static ChromiumWebBrowser Browser;
+        //public static ChromiumWebBrowser Browser;
 
         public Result Execute(
           ExternalCommandData commandData,
@@ -38,85 +26,9 @@ namespace SpeckleRevitPlugin
             DockablePaneId m_dpID = GlobalHelper.MainDockablePaneId;
             DockablePane m_dp = commandData.Application.GetDockablePane(m_dpID);
 
-
-            //var path = Directory.GetParent(Assembly.GetExecutingAssembly().Location);
-            //Debug.WriteLine(path, "SPK");
-
-            //var indexPath = string.Format(@"{0}\app\index.html", path);
-
-            //if (!File.Exists(indexPath))
-            //    Debug.WriteLine("Speckle for Revit: Error. The html file doesn't exists : {0}", "SPK");
-
-            //indexPath = indexPath.Replace("\\", "/");
-
-            //AppMain.MainDock.webBrowser.Source = new Uri( indexPath);
-            
-
-            //Init Cef
-            InitializeCef();
-            // initialise one browser instance
-            InitializeChromium();
-
-            var form = new SpeckleRevitForm();
-            form.Controls.Add(Browser);
-            form.Show();
-
-            //m_dp.Show();
+            m_dp.Show();
 
             return Result.Succeeded;
         }
-     
-        void InitializeCef()
-        {
-
-            Cef.EnableHighDPISupport();
-
-            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
-            var assemblyPath = Path.GetDirectoryName(assemblyLocation);
-            var pathSubprocess = Path.Combine(assemblyPath, "CefSharp.BrowserSubprocess.exe");
-            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
-            var settings = new CefSettings
-            {
-                LogSeverity = LogSeverity.Verbose,
-                LogFile = "ceflog.txt",
-                BrowserSubprocessPath = pathSubprocess
-            };
-
-            // Initialize cef with the provided settings
-
-            Cef.Initialize(settings);
-
-        }
-        public void InitializeChromium()
-        {
-
-#if DEBUG
-
-            Browser = new ChromiumWebBrowser(@"http://localhost:9090/");
-
-#else
-        var path = Directory.GetParent(Assembly.GetExecutingAssembly().Location);
-        Debug.WriteLine(path, "SPK");
-
-        var indexPath = string.Format(@"{0}\app\index.html", path);
-
-        if (!File.Exists(indexPath))
-            Debug.WriteLine("Speckle for Revit: Error. The html file doesn't exists : {0}", "SPK");
-
-        indexPath = indexPath.Replace("\\", "/");
-
-        Browser = new ChromiumWebBrowser(indexPath);
-#endif
-            //Allow the use of local resources in the browser
-            Browser.BrowserSettings = new BrowserSettings
-            {
-                FileAccessFromFileUrls = CefState.Enabled,
-                UniversalAccessFromFileUrls = CefState.Enabled
-            };
-
-
-            //Browser.Dock = DockStyle.Fill;
-        }
-        
     }
 }
